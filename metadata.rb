d95b86ed6675e0c285e124fbce758d214d6f3090
Bethany Erskine <bethany@paperlesspost.com>
2013-09-24 12:40:13 -0400

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer_email 'bethany@paperlesspost.com'
 license          'All rights reserved'
 description      'Installs/Configures sensu-tutorial'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
+depends "monitor"
+depends "sensu"
